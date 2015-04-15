ae146e461d8a1cb471fe598a48b694cda58650a0
xsauce <xsauce@gmail.com>
2015-04-15 09:09:38 +0800

    Create pearson.py
    判断两组数据与某一个直线拟合程度的度量，在数据不是很规范的时候，可以给出更好的结果。

---
+++ pearson.py
@@ -0,0 +1,18 @@
+def pearson(v1, v2):
+    si = {}
+    for i in v1:
+        if i in v2:
+            si[i] = 1
+    n = len(si)
+    if n == 0:
+        return 1
+    sum1 = sum([v1[i] for i in v1])
+    sum2 = sum([v2[i] for i in v2])
+    sum1Sq = sum([pow(v1[i], 2) for i in v1])
+    sum2Sq = sum([pow(v2[i], 2) for i in v2])
+    pSum = sum(v1[i] * v2[i] for i in v1])
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+    return num / den
